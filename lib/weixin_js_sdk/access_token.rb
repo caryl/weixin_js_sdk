5ab36cc9c4571503ba1b7a0cbc25d7482958d7ac
王宪伟 <caryl@users.noreply.github.com>
2015-05-21 17:44:38 +0800

    Update access_token.rb
    ruby 1.9语法

--- lib/weixin_js_sdk/access_token.rb
+++ lib/weixin_js_sdk/access_token.rb
@@ -2,9 +2,9 @@ module WeixinJsSDK
   class AccessToken
     URI_TEMPLATE = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%{app_id}&secret=%{app_secret}'.freeze
 
-    def initialize(app_id: '', app_secret: '')
-      @app_id = app_id
-      @app_secret = app_secret
+    def initialize(ps={})
+      @app_id = ps[:app_id] || ''
+      @app_secret = ps[:app_secret] || ''
     end
 
     def fetch
